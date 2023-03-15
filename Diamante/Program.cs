4c71c5857399f91a2f91a99585e7c7fa5acfd81a
rafae <rafae@DESKTOP-1PD5OK7>
2023-03-15 14:31:10 -0300

    Add project files.

---
+++ Diamante/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = 0;
+while (n % 2 == 0)
+{
+    Console.Write("Digite um número ímpar: ");
+    n = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < n; i++)
+{
+    if (i <= n / 2)
+    {
+        Console.WriteLine(new string(' ', (n / 2) - i) + new string('x', 2 * i + 1));
+    }
+    else
+    {
+        Console.WriteLine(new string(' ', i - (n / 2)) + new string('x', 2 * (n - i) - 1));
+    }
+}
